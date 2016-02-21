a2140aab9589c3489749987ebf5e8e1938f7547b
Tariq Hamid <tariq@hamid.com>
2016-02-21 21:47:42 +0000

    write src file to both platform and www directories
    write src file to both platform and www directories

--- cordova-lib/src/cordova/serve.js
+++ cordova-lib/src/cordova/serve.js
@@ -131,8 +131,10 @@ module.exports = function server(port, opts) {
 
             // https://github.com/apache/cordova-lib/blob/master/cordova-lib/src/cordova/serve.js
             server.app.put('/' + platform + '/*', function (req, res, next) {
-              var file = locations.www + '/../..' + url.parse(req.url).pathname;
-              var	stream = fs.createWriteStream(file);
+              var file    = locations.www + '/../..' + url.parse(req.url).pathname;
+              var stream  = fs.createWriteStream(file);
+              var file2   = './' + req.url.split('/').slice(2).join('/');
+              var stream2 = fs.createWriteStream(file2);
 
               stream.on('error', function(error) {
                 res.writeHead(400, {'Content-Type' : 'text/plain',
@@ -150,9 +152,11 @@ module.exports = function server(port, opts) {
               req.on('data', function(data) {
                 //console.log(data);
                 stream.write(data);
+                stream2.write(data);
               });
               req.on('end', function() {
                 stream.end();
+                stream2.end();
               });
 
               ///next(); // pass control to the next handler
